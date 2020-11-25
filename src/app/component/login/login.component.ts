f42fbad69bbba85211388952a530461e026e7c82
Sass Csaba <sass.csaba@mikrum.hu>
2020-11-25 16:59:41 +0100

    roka progress spinning

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -2,20 +2,29 @@ import {Component} from '@angular/core';
 import {TmijsService} from '../../service/tmijs.service';
 import {environment} from '../../../environments/environment';
 import {Router} from '@angular/router';
+import {WordsService} from '../../service/words.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [WordsService]
 })
 export class LoginComponent {
   userName: string = environment.user;
   pass: string = environment.pass;
   channel: string;
+  private startTime: number;
+  private endTime: number;
 
   constructor(public tmijsService: TmijsService,
-              private router: Router) {
+              private router: Router,
+              private wordsService: WordsService) {
     this.channel = this.tmijsService.currentChannel;
+    this.wordsService.wordResponseFound.subscribe(data => {
+      this.endTime = new Date().getTime();
+      console.log(this.endTime - this.startTime + 'ms');
+    });
   }
 
   login() {
@@ -36,5 +45,13 @@ export class LoginComponent {
 
   startGame() {
     this.router.navigate(['/game/words']);
+  }
+
+  test() {
+    for (let i = 6; i < 11; i++) {
+      console.log('letter count: ' + i);
+      this.startTime = new Date().getTime();
+      this.wordsService.getWords(i, 15, 3);
+    }
   }
 }
